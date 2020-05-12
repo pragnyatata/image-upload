0d7a33015815932251b6433cb0b8625f7162af62
tata2501 <pragnya25012000@gmail.com>
2020-05-12 19:41:17 +0530

    readme file

--- heplers/multerUpload.js
+++ heplers/multerUpload.js
@@ -14,13 +14,7 @@ exports.upload = multer({
   storage: storage,
   fileFilter: function (req, file, callback) {
     var ext = path.extname(file.originalname);
-    if (
-      ext !== ".png" &&
-      ext !== ".jpg" &&
-      ext !== ".gif" &&
-      ext !== ".jpeg" &&
-      ext !== ".png"
-    ) {
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
       return callback(new Error("Only images are allowed"));
     }
     callback(null, true);
